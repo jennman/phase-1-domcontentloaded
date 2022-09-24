0d9f9d627e76af509a789af18277b29cab51cbb8
Jennifer Man <jenniferman702@gmail.com>
2022-09-24 16:34:15 -0400

    commit tree ce70726119073d139cf9f373a3db2bfc11e0f09d

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded",function(){
+    console.log("The DOM has loaded");
+     document.getElementById('text').innerHTML = 'This is really cool!';
+});
+
+// let shorterText = document.getElementById('text').innerHTML;
+// function newText(shorterText){
+//    return "This is really cool!";
+// }
+
+// example for callback
+// $(document).ready(function() {
+//     $("a").click(function(event) {
+//         alert(event.target.id);
+//     });
+// });
+
+// let text = document.getElementById('text').innerHTML = 'This is really cool!'
+
+// const text = document.querySelector('p').innerHTML;
+
+// function changeText(p){
+//     p.textConent = 'This is really cool!';
+//     return changeText; 
+// }
+
